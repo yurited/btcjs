4459ae25b0165d530411f7fcc81f8278040a0e8c
Li Yu <ly257@yahoo-inc.com>
2016-11-02 16:35:30 -0700

    error handling and a bit of es6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* jshint node:true */
-
 'use strict';
 
 var http = require('http');
@@ -7,82 +5,99 @@ var async = require('async');
 var colors = require('colors');
 var getObjValue = require('../lib/get_obj_value');
 
-
-function outputPrice() {
-    async.parallel([
-        function(cb) {
-            http.get({
-                host: 'api.coindesk.com',
-                path: '/v1/bpi/currentprice/CNY.json'
-            }, function(response) {
-                var body = '';
-                response.on('data', function(d) {
-                    body += d;
-                });
-                response.on('end', function() {
-                    var parsed = JSON.parse(body);
-                    var USD = getObjValue(parsed, ['bpi', 'USD', 'rate_float']);
-                    var CNY = getObjValue(parsed, ['bpi', 'CNY', 'rate_float']);
-                    if (!USD || !CNY) {
-                        cb('Data error from coindesk.', null);
-                        return;
-                    }
-                    var ret = {
-                        usd: USD,
-                        cny: CNY,
-                        cny_usd_btc: CNY / USD
-                    };
-                    cb(null, ret);
-                });
-            });
-        },
-        function(cb) {
-            http.get({
-                host: 'query.yahooapis.com',
-                path: '/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCNY%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-            }, function(response) {
-                var body = '';
-                response.on('data', function(d) {
-                    body += d;
-                });
-                response.on('end', function() {
-                    var parsed = JSON.parse(body);
-                    var ret = getObjValue(parsed, ['query', 'results', 'rate', 'Rate']);
-                    if (!ret) {
-                        cb('Data error from yahoo.', null);
-                        return;
-                    }
-                    cb(null, ret);
-                });
-            });
-
+let coindeskService = (cb) => {
+    http.get({
+        host: 'api.coindesk.com',
+        path: '/v1/bpi/currentprice/CNY.json'
+    }, (response) => {
+        const statusCode = response.statusCode;
+        let error;
+        if (statusCode !== 200) {
+            error = new Error(`Request Failed.\n` + `Status Code: ${statusCode}`);
         }
-    ], function(err, results) {
-        if (err) {
-            console.error('Failed.', err);
+        if (error) {
+            response.resume();
             return;
         }
-        var cny_usd = parseFloat(results[1]);
-        var cny_usd_btc = results[0].cny_usd_btc;
-        var diff = (cny_usd_btc / cny_usd - 1) * 100;
-        var isUp = diff >= 0;
-        diff = ('(' + (isUp ? '+' + diff.toFixed(2) : diff.toFixed(2)) + '%)');
-        diff = isUp ? diff : diff.red;
-        console.log('\tCNY = ¥ ' + results[0].cny + ' / BTC');
-        console.log('\tUSD = $ ' + colors.black.bgGreen(results[0].usd) + ' / BTC');
-        console.log('\tCNY/USD = ' + results[1]);
-        console.log(
-            '\tCNY/USD based on BTC = ' +
-            results[0].cny_usd_btc.toFixed(4) + ' ' +
-            diff
-        );
-        // if (cny_usd_btc <= cny_usd + 0.01) {
-        //     console.log('\tCNY/USD based on BTC = ' + results[0].cny_usd_btc.toFixed(4).red);
-        // } else {
-        //     console.log('\tCNY/USD based on BTC = ' + results[0].cny_usd_btc.toFixed(4));
-        // }
-        console.log('Data provided by coindesk.com and Yahoo! Finance.\n'.gray)
-    });
-}
 
-exports.outputPrice = outputPrice;
+        let body = '';
+        response.on('data', (data) => {
+            body += data;
+        });
+        response.on('end', () => {
+            let parsed = JSON.parse(body);
+            let USD = getObjValue(parsed, ['bpi', 'USD', 'rate_float']);
+            let CNY = getObjValue(parsed, ['bpi', 'CNY', 'rate_float']);
+            if (!USD || !CNY) {
+                cb('Data error from coindesk.', null);
+                return;
+            }
+            let ret = {
+                usd: USD,
+                cny: CNY,
+                cny_usd_btc: CNY / USD
+            };
+            cb(null, ret);
+        });
+    }).on('error', (err) => {
+        cb('Request failed from coindesk api.', null);
+    });
+};
+
+let yahooService = (cb) => {
+    http.get({
+        host: 'query.yahooapis.com',
+        path: '/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCNY%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    }, (response) => {
+        const statusCode = response.statusCode;
+        let error;
+        if (statusCode !== 200) {
+            error = new Error(`Request Failed.\n` + `Status Code: ${statusCode}`);
+        }
+        if (error) {
+            response.resume();
+            return;
+        }
+        let body = '';
+        response.on('data', (data) => {
+            body += data;
+        });
+        response.on('end', () => {
+            let parsed = JSON.parse(body);
+            let ret = getObjValue(parsed, ['query', 'results', 'rate', 'Rate']);
+            if (!ret) {
+                cb('Data error from yahoo.', null);
+                return;
+            }
+            cb(null, ret);
+        });
+    }).on('error', (err) => {
+        cb('Request failed from yahoo api.', null);
+    });
+};
+
+exports.outputPrice = () => {
+    async.parallel(
+        [coindeskService, yahooService],
+        (err, results) => {
+            if (err) {
+                console.error(('Failed. ' + err).red);
+                return;
+            }
+            let cny_usd = parseFloat(results[1]);
+            let cny_usd_btc = results[0].cny_usd_btc;
+            let diff = (cny_usd_btc / cny_usd - 1) * 100;
+            let isUp = diff >= 0;
+            diff = ('(' + (isUp ? '+' + diff.toFixed(2) : diff.toFixed(2)) + '%)');
+            diff = isUp ? diff : diff.red;
+            console.log('\tCNY = ¥ ' + results[0].cny + ' / BTC');
+            console.log('\tUSD = $ ' + colors.black.bgGreen(results[0].usd) + ' / BTC');
+            console.log('\tCNY/USD = ' + results[1]);
+            console.log(
+                '\tCNY/USD based on BTC = ' +
+                results[0].cny_usd_btc.toFixed(4) + ' ' + diff
+            );
+            console.log('Data provided by coindesk.com and Yahoo! Finance.\n'.gray);
+        }
+    );
+}
